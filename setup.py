95dd34b93b5b37a1456b2c46a7099bf019d5aa6f
Kumar Anirudha <anirudhastark@yahoo.com>
2015-10-04 07:21:46 +0530

    version v1.0.0a2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0a1',
+    version='1.0.0a2',
 
     description='Django comment module using in reactJS',
     long_description='Django comment module using in reactJS. Currently only stores to db and uses a dummy user and post.',
